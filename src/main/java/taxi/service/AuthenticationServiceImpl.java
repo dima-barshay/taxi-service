3f8a241fdfc8d83c0579b837ed66d950140f0a2e
Dima Barshay <barshayerum8@gmail.com>
2021-12-16 15:30:15 +0200

    taxi service was implemented

---
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -0,0 +1,28 @@
+package taxi.service;
+
+import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import taxi.exception.AuthenticationException;
+import taxi.lib.Inject;
+import taxi.lib.Service;
+import taxi.model.Driver;
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Logger logger = LogManager.getLogger(AuthenticationServiceImpl.class);
+
+    @Inject
+    private DriverService driverService;
+
+    @Override
+    public Driver login(String login, String password) throws AuthenticationException {
+        Optional<Driver> driver = driverService.findByLogin(login);
+        if (driver.isEmpty() || !driver.get().getPassword().equals(password)) {
+            logger.warn("Authentication failed. Params: login={}", login);
+            throw new AuthenticationException("Login or password is incorrect");
+        }
+        logger.info("Authentication was successful. Params: login={}", login);
+        return driver.get();
+    }
+}
